2107a3cbba5871c45c21c4312f06c400903204d5
AMGIdhem <mahdi.bouhouch7@gmail.com>
2025-12-23 11:07:16 +0100

    product-service: controller - service - repository

---
+++ product-service/src/main/java/com/amgidhem/product_service/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.amgidhem.product_service.repository;
+
+import com.amgidhem.product_service.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+}
