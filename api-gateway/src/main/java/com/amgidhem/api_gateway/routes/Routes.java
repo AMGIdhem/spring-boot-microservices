c682c467fc822bbdddc699a49f0c5601f88f7f3c
AMGIdhem <mahdi.bouhouch7@gmail.com>
2025-12-23 14:54:19 +0100

    remove unused imports

--- api-gateway/src/main/java/com/amgidhem/api_gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/amgidhem/api_gateway/routes/Routes.java
@@ -1,10 +1,7 @@
 package com.amgidhem.api_gateway.routes;
 
-import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
-import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
